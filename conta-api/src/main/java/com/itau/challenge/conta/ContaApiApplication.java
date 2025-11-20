ba0bd01aa9533aa09617ed4940dccfe132843240
ton8080 <washingtonzxy@gmail.com>
2025-11-19 22:26:37 -0300

    chore: initial project structure

---
+++ conta-api/src/main/java/com/itau/challenge/conta/ContaApiApplication.java
@@ -0,0 +1,13 @@
+package com.itau.challenge.conta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaApiApplication.class, args);
+	}
+
+}
---
+++ conta-api/src/test/java/com/itau/challenge/conta/ContaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.challenge.conta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
